cd4eeb09a382289dd174f6eae90ac7000403eb0e
SmallGyro <860573094@qq.com>
2020-03-18 09:05:36 +0800

    other add Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("other Test print");
+System.out.println("other Test print");
+System.out.println("other Test print");
+System.out.println("other Test print");
+System.out.println("other Test print");
+System.out.println("other Test print");
+System.out.println("other Test print");
+    }
+}
